d09c725a0cfacec804536cbec10870b9971d4ae9
DJ Zaragoza <zaragoza0126@gmail.com>
2019-04-12 11:56:30 -0700

    continuing with project for both objects and arrays

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -23,8 +23,8 @@
 
 // Stretch
 
-exampleArray = [1,2,3,4];
-const triple = exampleArray.map( (num) => {
-  return num * 3;
-});
-console.log(triple);
+// exampleArray = [1,2,3,4];
+// const triple = exampleArray.map( (num) => {
+//   return num * 3;
+// });
+// console.log(triple);
